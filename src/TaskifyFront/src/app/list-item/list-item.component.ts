936804902fe56a88796410ac25fda2ad7d5f8f80
ankitdey96 <devankit104@gmail.com>
2025-01-12 22:36:37 +0600

    board title worked

--- src/TaskifyFront/src/app/list-item/list-item.component.ts
+++ src/TaskifyFront/src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -9,4 +9,25 @@ import { Component, Input } from '@angular/core';
 })
 export class ListItemComponent {
   @Input() HeaderName:string = '';
+  IsEditing:boolean = false;
+  constructor(private elementRef: ElementRef) {}
+
+  OnSubmit(NewName:string){
+    this.HeaderName = NewName.trim() || this.HeaderName; // Update the name only if valid
+
+  }
+
+  ButtonEditing(){
+    this.IsEditing = true;
+  }
+
+   @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent) {
+      debugger
+      const clickedInside = this.elementRef.nativeElement.contains(event.target);
+      if (!clickedInside) {
+        this.IsEditing = false;
+  
+      }
+    }
 }
